c26a54d9867cc244cb031697b783638ac091fcb0
Catalina <samplecatalina@gmail.com>
2023-12-18 23:52:13 -0500

    drafted up back end and starting front end

---
+++ ecom/src/main/java/com/proj/ecom/dao/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.proj.ecom.dao;
+
+import com.proj.ecom.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
---
+++ ecom/src/main/java/com/proj/ecom/entity/ProductCategory.java
@@ -0,0 +1,32 @@
+package com.proj.ecom.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "product_category")
+@Getter
+@Setter
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+//    @ManyToOne
+//    @JoinColumn (name = "category_id", nullable = false)
+//    private ProductCategory category;
+
+
+    @Column (name = "category_name")
+    private String category_name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
+
+
+}
