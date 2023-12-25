b845c959195409b2a267dea384370de292267dcc
Catalina <samplecatalina@gmail.com>
2023-12-25 14:50:47 -0500

    added feature: basic pagination

--- frontend/ecom-front/src/app/components/product-list/product-list.component.ts
+++ frontend/ecom-front/src/app/components/product-list/product-list.component.ts
@@ -13,7 +13,13 @@ export class ProductListComponent {
 
   products: Product[] = [];
   currentCategoryId: number = 1;
+  previousCategoryId: number = 1;
   searchMode: boolean = false;
+
+  // properties for server side paging
+  thePageNumber: number = 1;
+  thePageSize: number = 10;
+  theTotalElements: number = 0;
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
@@ -88,11 +94,31 @@ export class ProductListComponent {
       this.currentCategoryId = 1;
     }
 
+    // check if we have a different category than previous
+    // Note: Angular will reuse a component if it is currently being viewed
+    // if we have a different category id than previous
+    // then set thePageNumber back to 1
+    if (this.previousCategoryId != this.currentCategoryId) {
+      this.thePageNumber = 1;
+    }
+
+    this.previousCategoryId = this.currentCategoryId;
+
+    console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`);
+
     // now get the products for the given category id
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-      data => {
-        this.products = data;
-      }
-    )    
+    this.productService.getProductListPaginate(
+      this.thePageNumber - 1,
+      this.thePageSize,
+      this.currentCategoryId)
+      .subscribe(
+        data => {
+          this.products = data._embedded.products;
+          this.thePageNumber = data.page.number + 1; // Spring Data REST is zero based
+          this.thePageSize = data.page.size;
+          // now set the total elements
+          this.theTotalElements = data.page.totalElements;
+        }
+      );   
   }
 }
--- frontend/ecom-front/src/app/services/product.service.ts
+++ frontend/ecom-front/src/app/services/product.service.ts
@@ -23,6 +23,16 @@ export class ProductService {
     return this.httpClient.get<Product>(productUrl);
   }
 
+  getProductListPaginate(thePage: number, 
+                          thePageSize: number, 
+                          theCategoryId: number): Observable<GetResponseProducts> {
+    // need to build URL based on category id 
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+                      + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
   getProductList(theCategoryId: number): Observable<Product[]> {
     // need to build URL based on category id 
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
@@ -53,6 +63,12 @@ export class ProductService {
 interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  },
+  page: {
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
   }
 }
 
