0a7acb1bac61b4084c053fd6de6559b2d17c6c4b
Catalina <samplecatalina@gmail.com>
2023-12-26 16:04:05 -0500

    enabled "add to cart" button in the detailed page

--- frontend/ecom-front/src/app/components/product-details/product-details.component.ts
+++ frontend/ecom-front/src/app/components/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../../common/product';
 import { ProductService } from '../../services/product.service';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../../services/cart.service';
+import { CartItem } from '../../common/cart-item';
 
 @Component({
   selector: 'app-product-details',
@@ -9,14 +11,21 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './product-details.component.css'
 })
 export class ProductDetailsComponent implements OnInit {
+
   product!: Product;
+  
   constructor(private productService: ProductService,
+              private cartService: CartService,
               private route: ActivatedRoute) { }
+
+  
   ngOnInit(): void {
     this.route.paramMap.subscribe(() => {
       this.handleProductDetails();
     });
   }
+
+  
   handleProductDetails() {
     // get the "id" param string. convert string to a number using the "+" symbol
     const theProductId: number = +this.route.snapshot.paramMap.get('id')!;
@@ -25,5 +34,11 @@ export class ProductDetailsComponent implements OnInit {
         this.product = data;
       }
     );
+  }
+
+  addToCart() {
+    console.log(`Adding to cart: ${this.product.name}, ${this.product.unitPrice}`);
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem);
   }
 }
